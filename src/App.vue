35aa3cc4277663c015254e7f8f1945605194b0d3
cdrishinski <cdrishinski@gmail.com>
2020-03-19 16:36:49 -0600

    adding bootsrap

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <Header />
+     <Header />
     <QuestionBox />
   </div>
 </template>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,11 @@
 <template>
-    <div>
-        <h1>Header</h1>
-    </div>
+  <div tabs>     
+    <b-nav>
+      <b-nav-item disabled><strong>Quiz App</strong></b-nav-item>
+      <b-nav-item disabled>Counter: 4/10</b-nav-item>
+    </b-nav>
+  </div>
 </template>
+<style scoped>
+
+</style>
